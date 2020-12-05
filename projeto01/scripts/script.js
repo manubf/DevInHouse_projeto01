c9a34ec2e94b52ed2a471ef4a46fe6996901c20c
manubf <40336369+manubf@users.noreply.github.com>
2020-12-05 18:01:12 -0300

    Add files via upload

---
+++ projeto01/scripts/script.js
@@ -0,0 +1,88 @@
+let novoRotulo = document.getElementById('myInput');
+let lista_afazeres = [];
+
+carregar_lista();
+
+function novaAtividade() {
+    let marcado = false;
+    let rotulo = novoRotulo.value;
+
+    if (rotulo.trim() === "") {
+        alert("você precisa inserir algo como tarefa.");
+        return;
+    } 
+    
+    criarElementoLista(rotulo, marcado);
+    lista_afazeres.push({rotulo, marcado});
+    salva_localstorage();
+    novoRotulo.value = "";
+}
+
+function salva_localstorage() {
+    localStorage.setItem('lista_afazeres', JSON.stringify(lista_afazeres));
+}
+
+function criarElementoLista(rotulo, marcado) {
+    let li = document.createElement('li'); 
+    li.textContent = rotulo;
+
+    let check = document.createElement('input');
+    check.type = 'checkbox';
+    if (marcado) {
+        check.checked = marcado;
+        li.style.textDecoration = 'line-through';
+    }
+    check.onchange = troca_bool;
+    
+    let apaga = document.createElement('img');
+    apaga.src = 'close.png'
+    apaga.onclick = function(){
+        li.remove();
+        let listaAfazeresDois = [];
+        let ulChildren = document.getElementById('mostra_lista').children;
+        for (let i = 0; i < ulChildren.length; i++) {
+            let rotulo = ulChildren[i].textContent;
+            let marcado = ulChildren[i].lastChild.checked; 
+            listaAfazeresDois.push({rotulo, marcado});
+        }
+        lista_afazeres = listaAfazeresDois;
+        salva_localstorage()
+
+    };
+    
+    li.append(apaga);
+    li.appendChild(check);
+
+    function troca_bool(){
+        if(check.checked === true){
+            li.style.textDecoration = 'line-through';
+        }
+        else{
+            li.style.textDecoration = 'none';
+        }
+        let listaAfazeresDois = [];
+        let ulChildren = document.getElementById('mostra_lista').children;
+        for (let i = 0; i < ulChildren.length; i++) {
+            let rotulo = ulChildren[i].textContent;
+            let marcado = ulChildren[i].lastChild.checked; 
+            listaAfazeresDois.push({rotulo, marcado});
+        }
+        lista_afazeres = listaAfazeresDois;
+        salva_localstorage()
+    }
+
+    
+    mostra_lista.appendChild(li);
+    
+}
+
+
+function carregar_lista() {
+    let lista = JSON.parse(localStorage.getItem('lista_afazeres'));
+    if (lista) {
+        lista_afazeres = lista;
+        for (let i = 0; i < lista_afazeres.length; i++) {
+            criarElementoLista(lista_afazeres[i].rotulo, lista_afazeres[i].marcado);
+        }
+    }
+}
